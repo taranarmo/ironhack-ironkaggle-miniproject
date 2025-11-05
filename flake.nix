376de46bf98cc77ab7fd4b0ddc0680380b97cd61
Sergey Volkov <taranarmo@gmail.com>
2025-11-05 11:33:59 +0100

    modify dev shell

--- flake.nix
+++ flake.nix
@@ -6,56 +6,55 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        
-        # Define the main Python packages that can be reused for building artifacts
-        mainPythonPackages = ps: with ps; [
-          ps.numpy
-          ps.pandas
-          ps.matplotlib
-          ps.seaborn
-          ps.plotly
-          ps.scikit-learn
-          ps.ipython
-        ];
-        
-        # Python environment with the main packages
-        # pythonEnv = pkgs.python311.withPackages mainPythonPackages;
-        
-        # Main packages including uv and jupyter for development
+
+        # Main packages for development including uv, jupyter, and zsh
         mainPackages = with pkgs; [
-          # pythonEnv
           uv
           jupyter
-          zsh 
+          zsh
+          marp-cli
         ];
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = mainPackages;
-          
+
+          # Shell hook to setup Python environment with uv
           shellHook = ''
-            zsh
+            echo "Entering development environment with uv-managed Python packages"
+            echo "Run 'uv sync' to install dependencies from pyproject.toml"
+            echo "Run 'uv run jupyter notebook' to start Jupyter"
+            export SHELL=${pkgs.zsh}/bin/zsh
+            exec zsh
           '';
         };
-        
+
         packages = {
-          # Main application package that can be built/reused
-          default = pkgs.python311Packages.buildPythonApplication {
-            pname = "IronKaggleProject";
-            version = "0.1.0";
+          # Package that can be built with uv-managed dependencies
+          default = pkgs.stdenv.mkDerivation {
+            name = "IronKaggleProject-uv-env";
             src = ./.;
-            pyproject = true;
-            build-system = [ pkgs.python311Packages.setuptools ];
-            propagatedBuildInputs = mainPythonPackages pkgs.python311Packages;
-            doCheck = false; # Skip tests for now
+
+            buildInputs = mainPackages;
+
+            installPhase = "true";
+            buildPhase = "true";
+            fixupPhase = "true";
+
+            dontConfigure = true;
+            doInstall = true;
           };
-          
-          # Python environment as a package for reuse
-          # pythonEnv = pythonEnv;
         };
-      });
+      }
+    );
 }
