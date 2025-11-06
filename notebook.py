b45b99b97d4d2556bb2a29875ffa5efd536e925f
Sergey Volkov <taranarmo@gmail.com>
2025-11-06 16:30:35 +0100

    proceed to baseline models

--- notebook.py
+++ notebook.py
@@ -56,11 +56,76 @@ plt.show()
 # %%
 df.plot.box()
 plt.show()
-# most columnss have densly distributed values except price and sqft_lot which have more dispersion to the right
+# most columnss have densly distributed values except price and sqft_lot which have more dispersion to the right, especially price
 
 # %%
 scaler = sklearn.preprocessing.StandardScaler()
-numerical_scaled = df[numerical_columns].copy()
-numerical_scaled[:] = scaler.fit_transform(df[numerical_columns])
-numerical_scaled.plot.box()
+df_scaled = df.copy()
+df_scaled[numerical_columns] = scaler.fit_transform(df[numerical_columns])
+df_scaled.plot.box()
 plt.show()
+
+# %% [markdown]
+# ## Baseline models
+
+# %%
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+def print_report(model, X_train, X_test, y_train, y_test):
+    y_pred_test = model.predict(X_test)
+    y_pred_train = model.predict(X_train)
+    print(
+        f"R² on train is {r2_score(y_train, y_pred_train):.4f}",
+        f"MSE: {mean_squared_error(y_train, y_pred_train):.4f}",
+    )
+    print(
+        f"R² on test is  {r2_score(y_test, y_pred_test):.4f}",
+        f"MSE: {mean_squared_error(y_test, y_pred_test):.4f}",
+    )
+
+# %%
+X = df.drop(columns="price")
+X_scaled = df_scaled.drop(columns="price")
+target = df["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    target,
+    shuffle=True,
+    random_state=42, # we will reuse the split for scaled inputs
+)
+X_train_scaled, X_test_scaled, y_train, y_test = train_test_split(
+    X_scaled,
+    target,
+    shuffle=True,
+    random_state=42,
+)
+unscaled_features = {
+    "X_train": X_train,
+    "X_test":  X_test,
+    "y_train": y_train,
+    "y_test":  y_test,
+}
+scaled_features = {
+    "X_train": X_train_scaled,
+    "X_test":  X_test_scaled,
+    "y_train": y_train,
+    "y_test":  y_test
+}
+
+# %%
+from sklearn.linear_model import LinearRegression
+
+lm = LinearRegression().fit(X_train, y_train)
+print("LinearRegression")
+print_report(lm, **unscaled_features)
+
+# %%
+from sklearn.neighbors import KNeighborsRegressor
+
+knn = KNeighborsRegressor().fit(X_train, y_train)
+print_report(knn, **unscaled_features)
+
+# %%
+
